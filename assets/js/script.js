c32d3a7178e0223e6c91ad854717c54158092ace
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-01-05 20:02:58 -0500

    added api call to get a random word list, turn it into an array, will pass to second api call for second api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
-function getData(word) {
-  fetch("https://wordsapiv1.p.rapidapi.com/words/" + word + "/frequency", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-      "x-rapidapi-key": "60c5dbb668msh1e563b9d339bd3ep1cea02jsn0849238ba190",
-    },
-  })
+var wordList = [];
+function getData(randomWord) {
+  var word = randomWord;
+  console.log(typeof randomWord, typeof word, word);
+  fetch(
+    "https://twinword-word-graph-dictionary.p.rapidapi.com/reference/?entry=" +
+      word,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
+        "x-rapidapi-key": "60c5dbb668msh1e563b9d339bd3ep1cea02jsn0849238ba190",
+      },
+    }
+  )
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
         response.json().then(function (data) {
-          console.log(data, data.results);
+          console.log(data);
         });
       }
     })
@@ -19,50 +25,39 @@ function getData(word) {
     });
 }
 
-getData("collaborate");
+function getArray(input) {
+  for (i = 0; i < input.length; i++) {
+    wordList.push(input[i]);
+  }
+}
 
-// function getRandomWord() {
-//   fetch(
-//     "https://wordsapiv1.p.rapidapi.com/words/?limit=10&frequencymin=1.74-3&hasDetails=hasDetails",
-//     {
-//       method: "GET",
-//       headers: {
-//         "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-//         "x-rapidapi-key": "60c5dbb668msh1e563b9d339bd3ep1cea02jsn0849238ba190",
-//       },
-//     }
-//   )
-//     .then(function (response) {
-//       if (response.ok) {
-//         console.log(response);
-//         response.json().then(function (data) {
-//           console.log(data, data.results);
-//         });
-//       }
-//     })
-//     .catch(function (err) {
-//       console.error(err);
-//     });
-// }
+function getRandom() {
+  var words;
+  //  fetch("https://random-word-api.herokuapp.com/word?number=10")
 
-// getRandomWord();
-fetch(
-  "https://wordsapiv1.p.rapidapi.com/words/?frequencymin=5&frequencymax=6&random=true",  {
+  fetch("https://random-words5.p.rapidapi.com/getMultipleRandom?count=5", {
     method: "GET",
     headers: {
-      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+      "x-rapidapi-host": "random-words5.p.rapidapi.com",
       "x-rapidapi-key": "60c5dbb668msh1e563b9d339bd3ep1cea02jsn0849238ba190",
     },
-  }
-)
-.then(function (response) {
-  if (response.ok) {
-    console.log(response);
-    response.json().then(function (data) {
-      console.log(data, data.results);
+  })
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data.length);
+          // words = JSON.stringify(data);
+          // console.log(words);
+          //getData(words);
+          getArray(data);
+          console.log(wordList);
+        });
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
     });
-  }
-})
-.catch(function (err) {
-  console.error(err);
-});
+}
+
+getRandom();
