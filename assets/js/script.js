c93fb683c8ebf409a31761e0c4ebd1c57c2473e4
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-01-06 17:18:06 -0500

    function added to sort words into arrays based on frequency score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 var wordList = [];
 
+var easy = [];
+var medium = [];
+var hard = [];
+
+// Sorts words based on frequency score
+function getFrequency(wordScore) {
+  if (wordScore.score > 4.5) {
+    easy.push(wordScore.word);
+  } else if (wordScore.score > 3) {
+    medium.push(wordScore.word);
+  } else if (wordScore.score > 0) {
+    hard.push(wordScore.score);
+  }
+
+  console.log(easy, medium, hard);
+}
+
+// Returns the frequency (0 - 7) of a word to rate it's difficulty
 function getFrequencyAPI() {
+  var word;
+  var freq;
   for (var i = 0; i < wordList.length; i++) {
     fetch(
       "https://wordsapiv1.p.rapidapi.com/words/" + wordList[i] + "/frequency",
@@ -16,10 +36,21 @@ function getFrequencyAPI() {
       .then(function (response) {
         if (response.ok && response.status == 200) {
           response.json().then(function (data) {
-            console.log(data);
+            if (data.frequency.zipf) {
+              chosenWord = data.word;
+              freq = data.frequency.zipf;
+              var wordScore = {
+                word: chosenWord,
+                score: freq,
+              };
+              console.log(wordScore);
+              getFrequency(wordScore);
+            }
           });
         } else {
-          console.log(response.status, response.statusText);
+          console.log(
+            "Error Code: " + response.status + "\n" + response.statusText
+          );
         }
       })
       .catch(function (err) {
@@ -28,12 +59,14 @@ function getFrequencyAPI() {
   }
 }
 
+// Returns a global array of words created with the random words api
 function getArray(input) {
   for (i = 0; i < input.length; i++) {
     wordList.push(input[i]);
   }
 }
 
+// Returns a list of random words
 function getRandomWord() {
   var words;
   //  fetch("https://random-word-api.herokuapp.com/word?number=10")
@@ -48,9 +81,6 @@ function getRandomWord() {
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          // words = JSON.stringify(data);
-          // console.log(words);
-          //getFrequencyAPI(words);
           getArray(data);
           console.log(wordList);
           getFrequencyAPI();
