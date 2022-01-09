3c3eae8be13e7539aa225a83908c712e3d6dbbe8
TC <teresa.j.campbell@gmail.com>
2022-01-09 09:59:17 -0500

    cleaned up speechApi js

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,42 +125,23 @@ enterButtonEl.addEventListener("click", setTimer);
 
 //onclick speech API 
 
-
-
-
-
-
   var key= '2b89fd9e589e45af8478db4356434374';
-  var src = 'Hello, world!';
+  var src = chosenWord;
   var hl = 'en-us';
   var voice = 'Linda';
-  //r: 0, 
   var c = 'mp3';
-  //f: '44khz_16bit_stereo',
-  //ssml: false, } )
+  var f = '44khz_16bit_stereo';
+  var ssml =  false;
 
 
 document.getElementById("speech-button").onclick = function talk () {
 
-  fetch("http://api.voicerss.org/?key=" + key + "&=hl=" + hl + "&c=" + c + "&src=" + src,  {
+  fetch("http://api.voicerss.org/?key=" + key + "&hl=" + hl + "&c=" + c + "&f=" + f + "&src=" + src,  {
     "method": "GET", 
     "headers": {
       "x-rapidapi-host": "voicerss-text-to-speech.p.rapidapi.com",
       "x-rapidapi-key": "2b89fd9e589e45af8478db4356434374", }
-
-      //VoiceRSS.speech({
-        //key: '2b89fd9e589e45af8478db4356434374',
-        //src: 'Hello, world!',
-        //hl: 'en-us',
-        //v: 'Linda',
-        //r: 0, 
-        //c: 'mp3',
-        //f: '44khz_16bit_stereo',
-        //ssml: false, } )
     
-   
-       
-  
   .then(response => {
     console.log(response);
   })
@@ -168,23 +149,7 @@ document.getElementById("speech-button").onclick = function talk () {
     console.error(err);
   } ) } 
   ) }
-  //.then(response => response.json("voice", chosenWord))
-  //.then(data => console.log(data))
-    
-
-//VoiceRSS.getAudio('Hello World', {}, (err, audio) => {
-  //if (err) console.error(err);
-  //else new Audio(audio).play();
-//});
-
-
-
-// promise implementation
-// you can pass the options as the second argument, as in the callback implementation
-//VoiceRSS.getAudio('Hello World')
-  //.then(audio => new Audio(audio).play())
-  //.catch(error => console.log(error));
-
+ 
 
 
 
